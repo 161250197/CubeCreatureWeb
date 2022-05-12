0294e3b61fb79be5cfc40238bffa31ab73cd4c5f
zlic <161250197@smail.nju.edu.cn>
2022-05-12 16:05:17 +0800

    ✨ feat(dependence): add Element+

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <DevTools />
 </template>
 
 <style>
-body {
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
   overflow: hidden;
+  margin: 0;
 }
 </style>
