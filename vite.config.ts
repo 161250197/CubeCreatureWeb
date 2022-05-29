1bd8c43fc72d73d1736c839043310f31dd5e0997
zlic <161250197@smail.nju.edu.cn>
2022-05-29 14:53:08 +0800

    🐎 ci(inspect): add inspect

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,9 +16,10 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    Inspect()
   ],
   base: '/CubeCreatureWeb/',
   build: {
     outDir: 'docs',
-  }
+  },
 });
