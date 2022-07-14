eecee6bf6396b843038b25c3151e7129361974a5
zlic <161250197@smail.nju.edu.cn>
2022-07-14 14:24:55 +0800

    🦄 refactor: lint code

--- src/components/DevTools/DeleteCube.vue
+++ src/components/DevTools/DeleteCube.vue
@@ -1,6 +1,12 @@
 <template>
-  <el-table :data="cubes" empty-text="现在没有方块，先去添加方块吧" max-height="500" @selection-change="handleSelectionChange"
-    @cell-mouse-enter="onCellMouseEnter" @cell-mouse-leave="onCellMouseLeave">
+  <el-table
+    :data="cubes"
+    empty-text="现在没有方块，先去添加方块吧"
+    max-height="500"
+    @selection-change="handleSelectionChange"
+    @cell-mouse-enter="onCellMouseEnter"
+    @cell-mouse-leave="onCellMouseLeave"
+  >
     <el-table-column type="selection" width="55" />
     <el-table-column label="x">
       <template #default="{ row }">
@@ -24,15 +30,26 @@
     </el-table-column>
   </el-table>
   <el-divider />
-  <el-button v-if="deleteCubeCount" type="primary" size="large" @click="onRemoveClick">删除 {{ deleteCubeCount }} 个方块
+  <el-button
+    v-if="deleteCubeCount"
+    type="primary"
+    size="large"
+    @click="onRemoveClick"
+    >删除 {{ deleteCubeCount }} 个方块
   </el-button>
-  <el-button v-else type="warning" size="large" disabled plain>请先选择要删除的方块</el-button>
+  <el-button v-else type="warning" size="large" disabled plain
+    >请先选择要删除的方块</el-button
+  >
 </template>
 
 <script setup lang="ts">
-import { Cube } from 'vue';
-import { SET_CUBE_IS_PROMPT, SET_CUBES_IS_SELECTED, DELETE_CUBES_ACTION } from '../../store/mutation-types';
-import { getStore } from '../../store/store';
+import { Cube } from "vue";
+import {
+  SET_CUBE_IS_PROMPT,
+  SET_CUBES_IS_SELECTED,
+  DELETE_CUBES_ACTION,
+} from "../../store/mutation-types";
+import { getStore } from "../../store/store";
 
 const store = getStore();
 const cubes = computed(() => store.state.cubes);
