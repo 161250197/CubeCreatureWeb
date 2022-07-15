2add5441a77041fc9698c1f293c950711cea58f7
zlic <161250197@smail.nju.edu.cn>
2022-07-15 13:34:46 +0800

    🦄 refactor: lint code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createApp } from 'vue';
-import { store, STORE_KEY } from './store/store';
-import App from './App.vue';
+import { createApp } from "vue";
+import { store, STORE_KEY } from "./store/store";
+import App from "./App.vue";
 
 const app = createApp(App);
 app.use(store, STORE_KEY);
 
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-app.mount('#app');
+app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-import Inspect from 'vite-plugin-inspect';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Inspect from "vite-plugin-inspect";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      imports: ['vue', 'vuex'],
+      imports: ["vue", "vuex"],
       resolvers: [ElementPlusResolver()],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    Inspect()
+    Inspect(),
   ],
-  base: '/CubeCreatureWeb/',
+  base: "/CubeCreatureWeb/",
   build: {
-    outDir: 'docs',
+    outDir: "docs",
   },
 });
