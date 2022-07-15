50749b4b9304cff872c6dcf41399a520e2d78c83
zlic <161250197@smail.nju.edu.cn>
2022-07-15 13:34:46 +0800

    🦄 refactor: lint code

--- src/store/constant.ts
+++ src/store/constant.ts
@@ -1,24 +1,24 @@
 // mutation
 
 // dev
-export const ADD_CUDE = 'addCude';
-export const DELETE_CUBES = 'deleteCubes';
-export const SET_ADD_CUBE = 'setAddCube';
-export const SET_CUBE_IS_PROMPT_DEV = 'setCubeIsPromptDev';
-export const SET_CUBES_IS_SELECTED_DEV = 'setCubesIsSelectedDev';
-export const SET_DEV_TOOL_TAB = 'setDevToolTab';
+export const ADD_CUDE = "addCude";
+export const DELETE_CUBES = "deleteCubes";
+export const SET_ADD_CUBE = "setAddCube";
+export const SET_CUBE_IS_PROMPT_DEV = "setCubeIsPromptDev";
+export const SET_CUBES_IS_SELECTED_DEV = "setCubesIsSelectedDev";
+export const SET_DEV_TOOL_TAB = "setDevToolTab";
 
 // cube cartoon
-export const SET_CUBE_MOVE_DIRECTION = 'setCubeMoveDirection';
-export const SET_CUBE_MOVE_DISTANCE = 'setCubeMoveDistance';
-export const SET_CUBE_MOVE_DELAY = 'setCubeMoveDelay';
-export const MOVE_CUBE = 'moveCube';
+export const SET_CUBE_MOVE_DIRECTION = "setCubeMoveDirection";
+export const SET_CUBE_MOVE_DISTANCE = "setCubeMoveDistance";
+export const SET_CUBE_MOVE_DELAY = "setCubeMoveDelay";
+export const MOVE_CUBE = "moveCube";
 
 // mode
-export const SET_GAME_MODE = 'setGameMode';
+export const SET_GAME_MODE = "setGameMode";
 
 // action
-export const DELETE_CUBES_ACTION = 'deleteCubesAction';
-export const MOVE_CUBES_ACTION = 'moveCubesAction';
-export const ADD_TO_LEFT_CUBE = 'addToLeftCube';
-export const ADD_TO_RIGHT_CUBE = 'addToRightCube';
+export const DELETE_CUBES_ACTION = "deleteCubesAction";
+export const MOVE_CUBES_ACTION = "moveCubesAction";
+export const ADD_TO_LEFT_CUBE = "addToLeftCube";
+export const ADD_TO_RIGHT_CUBE = "addToRightCube";
--- src/util/constant.ts
+++ src/util/constant.ts
@@ -1,25 +1,25 @@
 /** 开发工具标签页 */
 export enum DevToolTabs {
-  add = '添加',
-  remove = '删除',
-  move = '滑动',
-  gameMode = '游戏模式',
-  kawaiMode = '(<ゝω·)~☆'
+  add = "添加",
+  remove = "删除",
+  move = "滑动",
+  gameMode = "游戏模式",
+  kawaiMode = "(<ゝω·)~☆",
 }
 
 /** 移动方向 */
 export enum MoveDirection {
-  no = '不移动',
-  right = '向右移动',
-  left = '向左移动',
-  down = '向下移动',
+  no = "不移动",
+  right = "向右移动",
+  left = "向左移动",
+  down = "向下移动",
 }
 
 /** 游戏模式 */
 export enum GameMode {
-  helloWorld = '你好！世界',
-  dev = '调试模式',
-  game = '游戏模式',
+  helloWorld = "你好！世界",
+  dev = "调试模式",
+  game = "游戏模式",
 }
 
 /** 移动动画时长 */
