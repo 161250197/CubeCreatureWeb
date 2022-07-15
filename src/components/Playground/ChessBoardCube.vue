d6fd3c53d9608c9ab18df27e4e697ea496b89c40
zlic <161250197@smail.nju.edu.cn>
2022-07-15 11:50:00 +0800

    🦄 refactor: abstract cube component

--- src/components/Playground/ChessBoardCube.vue
+++ src/components/Playground/ChessBoardCube.vue
@@ -17,18 +17,7 @@
     }"
     @click="onCubeClick"
   >
-    <span
-      class="face front"
-      :style="{ background, transform: transformFront }"
-    ></span>
-    <span
-      class="face left"
-      :style="{ background, transform: transformLeft }"
-    ></span>
-    <span
-      class="face top"
-      :style="{ background, transform: transformTop }"
-    ></span>
+    <CubeComponent :background="background" />
     <slot />
   </div>
 </template>
@@ -36,12 +25,7 @@
 <script setup lang="ts">
 import { Cube } from "vue";
 import { cubeRowCount } from "../../util/cube";
-import {
-  CUBE_SIZE,
-  pxSuffix,
-  CUBE_SIZE_PX,
-  HALF_CUBE_SIZE_PX,
-} from "../../util/constant";
+import { CUBE_SIZE, pxSuffix, CUBE_SIZE_PX } from "../../util/constant";
 
 const props = defineProps({
   cube: Object, // 暂时不支持复杂对象类型
@@ -73,10 +57,6 @@ const transformCube = computed(() => {
   return `translate3d(${transformY}, ${transformZ}, ${transformX})`;
 });
 
-const transformFront = `translateZ(${HALF_CUBE_SIZE_PX})`;
-const transformLeft = `rotateY(-90deg) translateZ(${HALF_CUBE_SIZE_PX})`;
-const transformTop = `rotateX(90deg) translateZ(${HALF_CUBE_SIZE_PX})`;
-
 function onCubeClick() {
   const validPromptCubeClick = props.isPrompt && props.isValidPrompt;
   if (validPromptCubeClick) {
@@ -90,49 +70,8 @@ function onCubeClick() {
   transform-style: preserve-3d;
   position: absolute;
 
-  .face {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    box-shadow: lightgrey -1px -1px 1px 0px, inset lightgrey -1px -1px 1px 0px;
-    opacity: 0.8;
-
-    &.left {
-      filter: brightness(1.05);
-    }
-
-    &.top {
-      filter: brightness(0.9);
-    }
-  }
-
   &.prompt {
     z-index: 1000 !important;
-
-    .face {
-      filter: brightness(1.5);
-    }
-  }
-
-  &.selected {
-    .face {
-      opacity: 1;
-      filter: brightness(0.8);
-      box-shadow: lightgrey -1px -1px 10px 0px,
-        inset lightgrey -1px -1px 1px 0px;
-    }
-  }
-
-  &.prompt {
-    .face {
-      opacity: 0.5;
-    }
-  }
-
-  &.to-delete {
-    .face {
-      animation: delete 0.5s linear 0s 1 forwards;
-    }
   }
 
   &.is-prompt {
@@ -141,20 +80,6 @@ function onCubeClick() {
     &.is-valid-prompt:hover {
       opacity: 1;
     }
-  }
-}
-
-@keyframes delete {
-  from {
-    opacity: 0.8;
-  }
-
-  10% {
-    opacity: 1;
-  }
-
-  to {
-    opacity: 0;
   }
 }
 
---
+++ src/components/Playground/CubeComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <span
+    class="face front"
+    :style="{ background, transform: transformFront }"
+  ></span>
+  <span
+    class="face left"
+    :style="{ background, transform: transformLeft }"
+  ></span>
+  <span
+    class="face top"
+    :style="{ background, transform: transformTop }"
+  ></span>
+</template>
+
+<script setup lang="ts">
+import { HALF_CUBE_SIZE_PX } from "../../util/constant";
+
+const props = defineProps({
+  background: String,
+});
+
+const transformFront = `translateZ(${HALF_CUBE_SIZE_PX})`;
+const transformLeft = `rotateY(-90deg) translateZ(${HALF_CUBE_SIZE_PX})`;
+const transformTop = `rotateX(90deg) translateZ(${HALF_CUBE_SIZE_PX})`;
+</script>
+
+<style scoped lang="less">
+.face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  box-shadow: lightgrey -1px -1px 1px 0px, inset lightgrey -1px -1px 1px 0px;
+  opacity: 0.8;
+
+  &.left {
+    filter: brightness(1.05);
+  }
+
+  &.top {
+    filter: brightness(0.9);
+  }
+}
+
+// #region ChessBoardCube 外层 class
+.prompt {
+  .face {
+    filter: brightness(1.5);
+  }
+}
+
+.selected {
+  .face {
+    opacity: 1;
+    filter: brightness(0.8);
+    box-shadow: lightgrey -1px -1px 10px 0px, inset lightgrey -1px -1px 1px 0px;
+  }
+}
+
+.prompt {
+  .face {
+    opacity: 0.5;
+  }
+}
+
+.to-delete {
+  .face {
+    animation: delete 0.5s linear 0s 1 forwards;
+  }
+}
+
+@keyframes delete {
+  from {
+    opacity: 0.8;
+  }
+
+  10% {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+}
+// #endregion
+</style>
